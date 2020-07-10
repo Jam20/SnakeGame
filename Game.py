1fcfb6cfa7634a1aedd43cb76a8635411ef55dcf
James <jbrouckman@gmail.com>
2020-07-09 20:06:32 -0400

    Started editing the game so it can be resized effectivly
    TODO
    -Fix pixel ammounts for resize bit
    -Start on Hamiltonian AI

--- Game.py
+++ Game.py
@@ -1,8 +1,11 @@
 import pygame
+from pygame.locals import *
 import random
 pygame.init()
-screen = pygame.display.set_mode([1000,1000])
+screen = pygame.display.set_mode((1000,1000), RESIZABLE)
 
+#Class which defines each section of the snake each with its own position
+#Singly Linked to the head of the snake and are drawn recursivly 
 class snakeBlock:
     def __init__(self, x, y):
         self.x = x
@@ -14,6 +17,8 @@ class snakeBlock:
         if(self.next != None):
             self.next.drawSelf()
 
+#Class which defines the apple which the snake needs
+#contains a position and a function to draw itself
 class appleObj:
     def __init__(self,x,y):
         self.x = x
@@ -21,7 +26,7 @@ class appleObj:
     def drawApple(self):
         pygame.draw.rect(screen, (255,0,0), (self.x*10, self.y*10, 10, 10))
 
-
+#Function used to draw the snake at the begining of the game
 def createSnake():
     head = snakeBlock(random.randint(10,90), random.randint(10,90))
     head.next = snakeBlock(head.x+1,head.y)
@@ -30,11 +35,16 @@ def createSnake():
     head.next.next.next.next = snakeBlock(head.x+4,head.y)
 
     return head
+
+#Function used to recursivly move the other segments of the snake
 def followPrev(prev):
     if(prev.next.next != None):
         followPrev(prev.next)
     prev.next.x = prev.x
     prev.next.y = prev.y
+
+#function used to move the head of the snake
+#calls followPrev() in order to move the rest of the snake
 def moveDirection(snake, dir):
     followPrev(snake)
     if dir == 0:
@@ -45,11 +55,15 @@ def moveDirection(snake, dir):
         snake.y+=1
     elif dir == 3:
         snake.x-=1
+
+#Function which adds a new segment to the snake when the apple is collected
 def addToSnake(snake):
     currentNode = snake
     while currentNode.next.next != None:
         currentNode = currentNode.next
     currentNode.next.next = snakeBlock(currentNode.next.x-currentNode.x,currentNode.next.y-currentNode.y)
+
+#Function which creates a new apple when one is collected
 def createNewApple(snake):
     newappX = random.randint(1,99)
     newappY = random.randint(1,99)
@@ -67,24 +81,28 @@ def createNewApple(snake):
         newappY = random.randint(1,99)
     return appleObj(newappX,newappY)
 
+#Function which checks to see if the snake has collided with the apple
 def checkAppleCollision(snake, apple):
     if(snake.x == apple.x and snake.y == apple.y):
         addToSnake(snake)
         return True
     return False
 
+#function which renders the text 
 def text_objects(text, font):
     textSurface = font.render(text, True, (0,0,0))
     return textSurface, textSurface.get_rect()
 
+#wrapper for the text function in order to abstract to a simple message
 def displayMessage(text,x,y):
     textFont = pygame.font.Font('freesansbold.ttf',30)
     TextSurf, TextRect = text_objects(text, textFont)
     TextRect.center = (x,y)
     screen.blit(TextSurf,TextRect)
 
-def checkSnakecollision(snake,dir):
-    if(snake.x>98 or snake.x<1 or snake.y<1 or snake.y>98):
+#function which checks if the snake has collided with itself
+def checkSnakecollision(snake,dir,size):
+    if(snake.x>size[0]/10 or snake.x<1 or snake.y<1 or snake.y>size[1]/10):
         return True
     else:
         currentNode = snake.next
@@ -95,28 +113,39 @@ def checkSnakecollision(snake,dir):
         return False
 
 
-pygame.display.set_caption("Snake")
 
+
+#Game Startup
+pygame.display.set_caption("Snake")
 running = True
 direction = 3
 snakeHead = createSnake()
 appleHead = createNewApple(snakeHead)
 score = 0
 timespeed = 100
+size = (1000,1000)
 aiControl = False
+
+#Game Loop
 while running:
     pygame.time.delay(timespeed)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == VIDEORESIZE:
+            screen = pygame.display.set_mode(event.dict['size'], RESIZABLE)
+            size = event.dict['size']
+            appleHead = createNewApple(snakeHead)
+            pygame.display.update()
 
     screen.fill((0,0,255))
-    pygame.draw.rect(screen, (255,0,0), (0,0,1000,10))
-    pygame.draw.rect(screen, (255,0,0), (0,0,10,1000))
-    pygame.draw.rect(screen, (255,0,0), (0,990,1000,10))
-    pygame.draw.rect(screen, (255,0,0), (990,0,10,1000))
+    pygame.draw.rect(screen, (255,0,0), (0,0,size[0],10))
+    pygame.draw.rect(screen, (255,0,0), (0,0,10,size[1]))
+    pygame.draw.rect(screen, (255,0,0), (0,size[1]-10,size[0],10))
+    pygame.draw.rect(screen, (255,0,0), (size[0]-10,0,10,size[1]))
     displayMessage("Score: " + str(score),900,25)
     displayMessage("Speed: " + str(timespeed),100,25)
+    print(size)
     snakeHead.drawSelf()
     appleHead.drawApple()
     pygame.display.update()
@@ -130,6 +159,8 @@ while running:
             direction = 2
         elif keys[pygame.K_a] and direction !=1:
             direction = 3
+    else:
+        direction = getNextMove(snakeHead,appleHead,direction,score)
     if keys[pygame.K_q]:
         if(timespeed<10):
             timespeed = timespeed-1
@@ -146,7 +177,7 @@ while running:
     if checkAppleCollision(snakeHead,appleHead):
         appleHead = createNewApple(snakeHead)
         score +=1
-    if checkSnakecollision(snakeHead,direction):
+    if checkSnakecollision(snakeHead,direction,size):
         snakeHead = createSnake()
         direction = 3
         appleHead = createNewApple(snakeHead)
